16b5ee02a3819ec1dee566247c68c71a1f867572
Bartek Dybowski <b .dybowski@gmail.com>
2020-08-07 13:02:50 +0200

    default config introduced

--- src/index.js
+++ src/index.js
@@ -2,20 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Emitter from 'tiny-emitter';
 import axios from 'axios';
-import { WebAnnotation, Editor, Environment, addPolyfills, setLocale } from '@recogito/recogito-client-core'; 
+import { WebAnnotation, Editor, Environment, addPolyfills, setLocale } from '@recogito/recogito-client-core';
 import ImageAnnotator from './ImageAnnotator';
+import defaultConfig from './config';
 
 addPolyfills(); // For Microsoft Edge
 
 import '@recogito/recogito-client-core/themes/default';
 
 /**
- * The entrypoint into the application. Provides the 
+ * The entrypoint into the application. Provides the
  * externally visible JavaScript API.
  */
 export class Annotorious {
 
-  constructor(config) {   
+  constructor(customConfig) {
+    // Initialize config using default configuration and
+    // overwrite it by the config provided by the user
+    const config = {
+      ...defaultConfig,
+      ...customConfig,
+    };
+
     // Programmatic calls to this instance from outside are forwarded
     // through a ref
     this._app = React.createRef();
@@ -23,11 +31,11 @@ export class Annotorious {
     // Event handling via tiny-emitter
     this._emitter = new Emitter();
 
-    // The image is wrapped in a DIV ('wrapperEl'). The application 
-    // container DIV, which holds the editor popup, will be attached 
+    // The image is wrapped in a DIV ('wrapperEl'). The application
+    // container DIV, which holds the editor popup, will be attached
     // as a child to the wrapper element (=a sibling to the image
     // element).
-    const imageEl = (config.image.nodeType) ? 
+    const imageEl = (config.image.nodeType) ?
       config.image : document.getElementById(config.image);
 
     // DIV will have unwanted margin at the bottom otherwise!
@@ -43,15 +51,15 @@ export class Annotorious {
     wrapperEl.style.display = 'inline-block';
     imageEl.parentNode.insertBefore(wrapperEl, imageEl);
     wrapperEl.appendChild(imageEl);
-    
+
     this._appContainerEl = document.createElement('DIV');
     wrapperEl.appendChild(this._appContainerEl);
 
     const { CommentWidget, TagWidget } = Editor;
-   
+
     // A basic ImageAnnotator with just a comment and a tag widget
     ReactDOM.render(
-      <ImageAnnotator 
+      <ImageAnnotator
         ref={this._app}
         imageEl={imageEl}
         wrapperEl={wrapperEl}
@@ -59,28 +67,28 @@ export class Annotorious {
         headless={config.headless}
         onSelectionCreated={this.handleSelectionCreated}
         onSelectionTargetChanged={this.handleSelectionTargetChanged}
-        onAnnotationCreated={this.handleAnnotationCreated} 
+        onAnnotationCreated={this.handleAnnotationCreated}
         onAnnotationSelected={this.handleAnnotationSelected}
-        onAnnotationUpdated={this.handleAnnotationUpdated} 
+        onAnnotationUpdated={this.handleAnnotationUpdated}
         onAnnotationDeleted={this.handleAnnotationDeleted}
         onMouseEnterAnnotation={this.handleMouseEnterAnnotation}
         onMouseLeaveAnnotation={this.handleMouseLeaveAnnotation}>
-        
-        <CommentWidget />
-        <TagWidget vocabulary={config.tagVocabulary} />
+
+        {config.allowComments && <CommentWidget />}
+        {config.allowTags && <TagWidget vocabulary={config.tagVocabulary} />}
 
       </ImageAnnotator>,
-    
+
     this._appContainerEl);
   }
 
   handleSelectionCreated = selection =>
     this._emitter.emit('createSelection', selection._stub);
 
-  handleSelectionTargetChanged = target => 
+  handleSelectionTargetChanged = target =>
     this._emitter.emit('changeSelectionTarget', target);
 
-  handleAnnotationSelected = annotation => 
+  handleAnnotationSelected = annotation =>
     this._emitter.emit('selectAnnotation', annotation.underlying);
 
   handleAnnotationCreated = annotation =>
@@ -97,11 +105,11 @@ export class Annotorious {
 
   handleMouseLeaveAnnotation = (annotation, evt) =>
     this._emitter.emit('mouseLeaveAnnotation', annotation.underlying, evt);
-  
-  /******************/               
+
+  /******************/
   /*  External API  */
-  /******************/     
-  
+  /******************/
+
   /**
    * Adds a single JSON-LD WebAnnotation to the annotation layer.
    */
@@ -114,7 +122,7 @@ export class Annotorious {
   removeAnnotation = annotation =>
     this._app.current.removeAnnotation(new WebAnnotation(annotation));
 
-  /** 
+  /**
    * Loads JSON-LD WebAnnotations from the given URL.
    */
   loadAnnotations = url => axios.get(url).then(response => {
@@ -146,15 +154,15 @@ export class Annotorious {
 
   /** Programmatically selects the annotation, opening the editor popup **/
   selectAnnotation = annotationOrId => {
-    const arg = (annotationOrId?.type === 'Annotation') ? 
+    const arg = (annotationOrId?.type === 'Annotation') ?
       new WebAnnotation(annotationOrId) : annotationOrId;
 
     const selected = this._app.current.selectAnnotation(arg);
     return selected?.underlying;
   }
 
-  /** 
-   * Applies an annotation template to newly created 
+  /**
+   * Applies an annotation template to newly created
    * selections. The editor will just apply the template and
    * stay closed, unless openEditor is se to true.
    */
@@ -178,19 +186,19 @@ export class Annotorious {
     Environment.user = null;
 
   /** Sets the current 'server time', to avoid problems with locally-generated timestamps **/
-  setServerTime = timestamp => 
+  setServerTime = timestamp =>
     Environment.setServerTime(timestamp);
 
-  /** 
+  /**
    * Adds an event handler.
    */
   on = (event, handler) =>
     this._emitter.on(event, handler);
 
-  /** 
+  /**
    * Removes an event handler.
-   * 
-   * If no callback, removes all handlers for 
+   *
+   * If no callback, removes all handlers for
    * the given event.
    */
   off = (event, callback) =>
